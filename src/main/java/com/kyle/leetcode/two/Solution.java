df06ebeeb9d163bedb7646a842c5156313400315
Kyle.K.Zhao <13636814471@163.com>
2020-04-04 22:50:59 +0800

    add four

--- src/main/java/com/kyle/leetcode/two/Solution.java
+++ src/main/java/com/kyle/leetcode/two/Solution.java
@@ -23,19 +23,65 @@ package com.kyle.leetcode.two;
  *     ListNode(int x) { val = x; }
  * }
  */
+class ListNode {
+    int val;
+    ListNode next;
 
+    ListNode(int x) {
+        this.val = x;
+    }
+}
 
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return null;
+        // 第一个节点可以不返回，直接返回result.next 即可
+        ListNode result = new ListNode(0);
+        ListNode data = result;
+        int pre = 0;
+        int val;
+        while (true) {
+            if (l1 == null) {
+                if (l2 == null) {
+                    break;
+                } else {
+                    val = l2.val + pre;
+                }
+            } else {
+                if (l2 == null) {
+                    val = l1.val + pre;
+                } else {
+                    val = l1.val + l2.val + pre;
+                }
+            }
+            if (val > 9) {
+                pre = 1;
+                val = val - 10;
+            } else {
+                pre = 0;
+            }
+            data.next = new ListNode(val);
+            data = data.next;
+            l1 = l1 == null ? l1 : l1.next;
+            l2 = l2 == null ? l2 : l2.next;
+        }
+        if (pre != 0) {
+            data.next = new ListNode(pre);
+        }
+        return result.next;
+    }
+    public static void main(String[] args) {
+        ListNode l1;
+        l1 = new ListNode(2);
+        l1.next = new ListNode(3);
+        l1.next.next = new ListNode(4);
+
+        ListNode l2;
+        l2 = new ListNode(8);
+        l2.next = new ListNode(1);
+        l2.next.next = new ListNode(6);
+        new Solution().addTwoNumbers(l1, l2);
     }
 
-    class ListNode {
-        int val;
-        ListNode(int x ) {
-            val = x;
-        }
-    }
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
